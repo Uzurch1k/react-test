6fba82047a97f521746c05d369107936e22042d0
Uzurch1k <galcenkovlad7@gmail.com>
2024-03-15 02:19:37 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 // src/components/App.jsx
 
 import Product from './Product';
+import Alert from './Alert';
+import BookList from './BookList';
 
 export default function App() {
   return (
-    <div>
+    <>
       <h1>Best selling</h1>
-
       <Product
         name="Tacos With Lime"
         imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
@@ -17,6 +18,21 @@ export default function App() {
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
         price={14.29}
       />
-    </div>
+
+      <BookList />
+
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+    </>
   );
 }
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,24 +1,19 @@
+// src/components/BookList.jsx
+
 const favouriteBooks = [
   { id: 'id-1', name: 'JS for beginners' },
   { id: 'id-2', name: 'React basics' },
   { id: 'id-3', name: 'React Router overview' },
 ];
 
-const BookList = ({ books }) => {
+const BookList = () => {
   return (
     <ul>
-      {books.map(book => {
+      {favouriteBooks.map(book => {
         return <li key={book.id}>{book.name}</li>;
       })}
     </ul>
   );
 };
 
-export const App = () => {
-  return (
-    <>
-      <h1>Books of the week</h1>
-      <BookList books={favouriteBooks} />
-    </>
-  );
-};
+export default BookList;
